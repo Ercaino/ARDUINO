3946d9b4b86a2f46e53659b3043a5779f46be9a6
Ercaino <86023301+Ercaino@users.noreply.github.com>
2023-11-24 16:51:24 +0000

    Add dht11 t-h

---
+++ Temperature_Humidity/DHT11_T_H.ino
@@ -0,0 +1,40 @@
+// Initialize Library
+#include <SimpleDHT.h>
+
+// for DHT11,
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 2
+
+// Define my object
+SimpleDHT11 dht11;
+
+// Pin data DHT11
+#define DHTPIN 2
+
+void setup() {
+  // Serial
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("=================================");
+  Serial.println("Sample DHT11...");
+
+  byte temp = 0;
+  byte umid = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(DHTPIN, &temp, &umid, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err=");
+    Serial.println(err);
+    delay(1000);
+    return;
+  }
+
+  Serial.print("Sample OK: ");
+  Serial.print((int)temp); Serial.print(" *C, ");
+  Serial.print((int)umid); Serial.println(" H");
+
+  // DHT11 sampling rate is 1HZ.
+  delay(1500);
+}
